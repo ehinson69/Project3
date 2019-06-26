c32e61052c35f5f25db4003b2496bbb7d4e0b2a0
Elizabeth Hinson <elizabeth.hinson@verizon>
2019-06-26 15:06:31 -0500

    trial and error on hide and show

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
     $('form:first *:input[type!=hidden]:first').focus();
 });
+$('.text').hide();
+        $('.text').show();
+        box.addEventListener('click', job_role_other() {
+          alert('Your Job Role');
+        });
