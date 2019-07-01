d2d8c8692e0be3ee9ccb49591b2c09de723e7fa7
Elizabeth Hinson <elizabeth.hinson@verizon>
2019-07-01 12:35:56 -0500

    corrections to section 1

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
-    $('form:first *:input[type!=hidden]:first').focus();
+  'other';
+$('#name').focus();
+$('other-title').append('input type="text" id="other-title" name="job_role_other" placeholder="Your Job Role"');
+$('other-title').hide();
+$('#title').change(function(){
+  if($('#title option:selected').val() === "other"){
+    $('other-title').show();
+  } else {
+    $('other-title').hide();
+  }
+}
 });
-$('.text').hide();
-        $('.text').show();
-        box.addEventListener('click', job_role_other() {
-          alert('Your Job Role');
-        });
