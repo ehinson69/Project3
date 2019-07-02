e98b3815e4db6be663a9f2daf4e628ad3f79a06a
Elizabeth Hinson <elizabeth.hinson@verizon>
2019-07-02 14:13:35 -0500

    added t-shirt section

--- js/script.js
+++ js/script.js
@@ -1,13 +1,36 @@
+//**Function to focus on Name field as soon as form is displayed.*/
 $(document).ready(function() {
-  'other';
-$('#name').focus();
-$('other').append('input type="text" id="other-title" name="job_role_other" placeholder="Your Job Role"');
-$('#user-title').click(function(){
-  $(".target").change();
-  if($('#title option:selected').val() === "Job Role"){
-    $('other').show();
-  } else {
-    $('other').hide();
+  $('#name').focus();
+//**Change function to show/hide Other field.*/
+$('#other-title').hide();
+  $('#title').change(function() {
+    if($('#title').val() == 'other'){
+      $('#other-title').show();
+    } else {
+      $('#other-title').hide();
   }
 })
+});
+
+//**T-shirt section*/
+//**Select a t-shirt design and a color will show/hide.*/
+$('#colors-js-puns').hide();
+
+var shirtSelection = false;
+$('#design').change(function(){
+  if($('design option:selection').val() === "js puns") {
+    $('#colors-js-puns').show();
+    $('#color').html('<option value="cornflowerblue">Cornflower Blue (js puns shirt only)</option>;
+    $('#color').html('<option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option>; 
+    $('#color').html('<option value="gold">Gold (JS Puns shirt only)</option>; 
+    $('#color').html('<option value="tomato">Tomato (I &#9829; JS shirt only)</option>;
+    $('#color').html('<option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option>; 
+    $('#color').html('<option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option>;
+  shirtSelection = true;
+  return shirtSelection;
+  } else {
+    $('#color-js-puns').hide();
+    shirtSelection = fals;
+    return shirtSelection;
+  }
 });
